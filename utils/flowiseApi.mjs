28d96ae55d115ee324b1e3e58153c02c41b3cb88
bernejorge <bernejorge@gmail.com>
2024-09-17 12:15:35 -0300

    flowiserApi.mjs con variables de entorno

--- utils/flowiseApi.mjs
+++ utils/flowiseApi.mjs
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'; // Asegúrate de que 'node-fetch' esté instalado
 
-const flowiseApiUrl = "http://149.50.142.145:3001/api/v1/prediction/feddcaa1-24d3-437f-a774-17dc917faa9c";
-const flowiseAuthToken = "Bearer qdMWjrAi0bhLyl_7KWkBHwrOtSEyCrHagMCtXvu9XTg";
+const flowiseApiUrl = process.env.FLOWISE_API_URL || "http://149.50.142.145:3001/api/v1/prediction/feddcaa1-24d3-437f-a774-17dc917faa9c";
+const flowiseAuthToken = process.env.FLOWISE_AUTH_TOKEN || "qdMWjrAi0bhLyl_7KWkBHwrOtSEyCrHagMCtXvu9XTg";
 
 export const queryFlowise = async (data) => {
     try {
         const response = await fetch(flowiseApiUrl, {
             headers: {
-                Authorization: flowiseAuthToken,
+                Authorization: 'Bearer ' + flowiseAuthToken,
                 "Content-Type": "application/json"
             },
             method: "POST",
